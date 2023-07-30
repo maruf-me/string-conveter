4ae945b76a1290f38078dae2e8d5a3e3a26a7917
devschapy <devschapy71@gmail.com>
2023-07-31 00:49:46 +0600

    created project and design home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import { convartStringToPascalCase } from "./utility/ConvartPascalCase";
 
-function App() {
+const App = () => {
+  const [userInput, setUserInput] = useState('');
+  const pascalCaseStr = convartStringToPascalCase(userInput);
+
+  const textRef = useRef(null);
+
+  const handleCopy = () => {
+    console.dir(textRef.current);
+    if (textRef.current) {
+      textRef.current.select();
+      document.execCommand('copy');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-100 h-screen flex items-center justify-center">
+      <div className="space-y-4">
+
+        <div className="space-y-1">
+          <label htmlFor="input" className="block">Input</label>
+          <input
+            id="input"
+            type="text"
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
+            className="border rounded-md py-2 px-3 shadow-sm focus:outline-none"
+          />
+        </div>
+
+
+        <div>
+          <p>Expected Value: { }</p>
+          <div className="bg-gray-200 p-2 rounded-lg shadow-sm border border-gray-300">
+
+            <textarea ref={textRef} className="break-all min-h-[10rem] w-[30rem] bg-gray-300 px-1 rounded focus:outline-green-200" value={pascalCaseStr} readOnly />
+            <div className={`${pascalCaseStr ? 'h-auto opacity-100 visible duration-500' : 'h-0 opacity-0 invisible'} w-full flex justify-end gap-4`}>
+
+              <button onClick={() => setUserInput('')} className="bg-red-300 px-3 py-1 rounded">Clear</button>
+              <button onClick={handleCopy} className="bg-green-300 px-3 py-1 rounded">Copy</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/utility/ConvartPascalCase.js
@@ -0,0 +1,15 @@
+
+export const convartStringToPascalCase = (str) => {
+    // Split the string into an array of words
+    const words = str.split(' ');
+
+    // Capitalize the first letter of each word
+    const capitalizedWords = words.map(word => {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+
+    // Join the words back together with underscores in between
+    const result = capitalizedWords.join('_');
+
+    return result;
+}
